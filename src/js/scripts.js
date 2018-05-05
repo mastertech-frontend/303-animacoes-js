28a2825c3e4ee2209d04e507960b03d84c6f50c3
Bruno Pimentel <bpimentel@mastertech.tech>
2018-05-04 21:49:48 -0300

    Finalizar exemplos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,3 @@
-let matrix = document.querySelector('#matrix');
-
-setInterval(function(){
-    let quantidade = 10 + Math.floor(Math.random() * 40);
-
-    for(let i = 0; i < quantidade; i ++){
-        let p = document.createElement('p');
-        p.innerHTML = 'lorem ipsum dolor sit amet';
-        p.style.left = Math.random() * window.innerWidth + 'px';
-        
-        let delay = Math.random();
-        let opacity = Math.random();
-        p.style.animationDelay = delay + 's';
-        p.style.opacity = opacity;
-        matrix.appendChild(p);
-
-        setTimeout(function(){
-            matrix.removeChild(p);
-        }, 5000);
-    }
-}, 1000);
 let mario = document.querySelector('img');
 let posicaoX = 0;
 let posicaoY = 0;
